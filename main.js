677149f3fa1476fa1c5e944a65aa7add911c0f97
Jéssica Limonge <jessicalimonge22@gmail.com>
2026-06-10 20:28:31 -0300

    criação da função para buscar dados

--- main.js
+++ main.js
@@ -4,3 +4,11 @@ const inputNome = document.getElementById("input-nome");
 const inputQuantidade = document.getElementById("input-quantidade");
 const btnCadastrar = document.getElementById("btn-cadastrar");
 const listaMateriais = document.getElementById("lista-materiais");
+async function buscarMateriais() {
+    try {
+        const response = await fetch(API_URL);
+        if (!response.ok) throw new Error("Erro ao carregar dados do servidor.");
+
+        const materiais = await response.json();
+    }
+    
