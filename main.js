35f1b0e9dcdb8763ada3c12444ceb35a3185f0f8
Jéssica Limonge <jessicalimonge22@gmail.com>
2026-06-17 19:32:04 -0300

    adição da função obrigatória

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const API_URL = "https://6a29d3bcf59cb8f65f1da5b8.mockapi.io/materiais";
-
+function validarRetirada(estoqueAtual, quantityRetirada) {
+    if (quantityRetirada <= 0) return false;
+    if (quantityRetirada > estoqueAtual) return false;
+    return true;
+}
 const inputNome = document.getElementById("input-nome");
 const inputQuantidade = document.getElementById("input-quantidade");
 const btnCadastrar = document.getElementById("btn-cadastrar");
